c941e66de58a5102aeb13af818d5ca417a448c7d
Igoris Ivanovas <igoris.x@inbox.lt>
2021-09-24 11:20:16 +0300

    start

---
+++ app/Http/Controllers/StatuseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Statuse;
+use Illuminate\Http\Request;
+
+class StatuseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Statuse  $statuse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Statuse $statuse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Statuse  $statuse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Statuse $statuse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Statuse  $statuse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Statuse $statuse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Statuse  $statuse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Statuse $statuse)
+    {
+        //
+    }
+}
